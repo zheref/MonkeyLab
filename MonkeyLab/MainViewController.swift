0edefebd1513c7a46dc97a0c72427f2b5a90a5ed
me@sergiodaniel.co <me@sergiodaniel.co>
2018-10-06 17:48:54 -0500

    Set up first time environment

---
+++ MonkeyLab/MainViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MainViewController.swift
+//  MonkeyLab
+//
+//  Created by Sergio Daniel L. García on 10/6/18.
+//  Copyright © 2018 Sergio Lozano García. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+#  About MonkeyLab
+
+## Frameworks to try out here:
+1. dnssd
+2. CallKit
+3. Messages
+4. MessagesUI
+5. SiriKit
+6. Speech
+7. UserNotifications
+8. UserNotificationsUI
